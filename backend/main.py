d731841f3bffcede97d2ce70e41431e7b20beed6
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2021-10-10 20:36:04 +0530

    fix: minor typo

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"message":"welcome to DevStation Backend !"}
+    return {"message":"welcome to project Backend !"}
 
 @app.on_event("startup")
 async def startup_db_client():
