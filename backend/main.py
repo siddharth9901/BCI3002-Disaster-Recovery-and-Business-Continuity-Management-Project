a07ebfdf006c2435c311ed29a478c8c51c3b1355
batman004 <pathania.yuvraj@gmail.com>
2021-10-10 20:32:55 +0530

    update: add base backend routes

---
+++ backend/main.py
@@ -0,0 +1,47 @@
+import uvicorn
+from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.user.routers import router as users_router
+
+from .config import settings
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message":"welcome to DevStation Backend !"}
+
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
+    app.mongodb = app.mongodb_client[settings.DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+app.include_router(users_router, tags=["users"], prefix="/user")
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        reload=settings.DEBUG_MODE,
+        port=settings.PORT,
+    )
